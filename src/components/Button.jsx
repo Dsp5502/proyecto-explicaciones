8c60d8420f48c89b4d2a386d68de5cc7e80960c8
David Puerto <dsp5502@gmail.com>
2021-11-08 12:07:04 -0500

    agregar 1

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = () => {
-  return <button className='button'>Este es mi boton</button>;
+  return <button className='button'>Este es mi boton 1</button>;
 };
 
 export default Button;
